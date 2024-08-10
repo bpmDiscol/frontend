fea0b40391f01b0bb099cf03d84afe980c93f7bb
Fierdimo <grmoralesp@gmail.com>
2024-08-10 10:29:39 -0500

    more options

--- imports/ui/misc/gearsOptions.js
+++ imports/ui/misc/gearsOptions.js
@@ -1,42 +1,46 @@
 export const gearOptions = [
-    {
-      label: "Computador de escritorio",
-      value: "isDesktopPC",
-    },
-    {
-      label: "Computador portatil",
-      value: "isLaptop",
-    },
-    {
-      label: "Monitor de computador",
-      value: "isScreen",
-    },
-    {
-      label: "Teclado",
-      value: "isKeyboard",
-    },
-    {
-      label: "Ratón",
-      value: "isMouse",
-    },
-    {
-      label: "Correo electrónico corporativo",
-      value: "isEmail",
-    },
-    {
-      label: "Escritorio",
-      value: "isDesk",
-    },
-    {
-      label: "Silla",
-      value: "isChair",
-    },
-    {
-      label: "Diadema",
-      value: "isHeadset",
-    },
-    {
-      label: "Teléfono Celular",
-      value: "isCelphone",
-    },
-  ];
+  {
+    label: "Computador de escritorio",
+    value: "isDesktopPC",
+  },
+  {
+    label: "Computador portatil",
+    value: "isLaptop",
+  },
+  {
+    label: "Monitor de computador",
+    value: "isScreen",
+  },
+  {
+    label: "Teclado",
+    value: "isKeyboard",
+  },
+  {
+    label: "Ratón",
+    value: "isMouse",
+  },
+  {
+    label: "Correo electrónico corporativo",
+    value: "isEmail",
+  },
+  {
+    label: "Escritorio",
+    value: "isDesk",
+  },
+  {
+    label: "Silla",
+    value: "isChair",
+  },
+  {
+    label: "Diadema",
+    value: "isHeadset",
+  },
+  {
+    label: "Teléfono Celular",
+    value: "isCelphone",
+  },
+  {
+    label: "EPP",
+    value: "EPP",
+  },
+];
